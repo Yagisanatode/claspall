915bb2f61d4525d3e8c2b2fc40e3ce79d242e287
yagisanatode <scott@yagisanatode.com>
2024-09-25 08:55:28 +0700

    Added short flags -ls -v

--- command.go
+++ command.go
@@ -10,10 +10,11 @@ import (
 
 // Command line flags
 type CmdFlags struct {
-	Add  string
-	Edit string
-	Del  int
-	List bool
+	Add     string
+	Edit    string
+	Del     int
+	List    bool
+	Version bool
 }
 
 var FlagUsage = struct {
@@ -25,9 +26,11 @@ var FlagUsage = struct {
 }{
 	Add: `
 Add a new Apps Script file location:
-- [how] Between quotation marks add a name or title then a colon (:) an the file id. Close quotation marks.
+- [how] Between quotation marks add a name or title then 
+    a colon (:) an the file id. Close quotation marks.
 - [syntax] -add "title:Apps-Script-File-Id"
-- [example] -add "Prod:1_hg5Lj-lOXbZMm60FizXSEZBmYN27-ozK-JOX4fRmEWntroxQ"
+- [example] 
+    claspall -add "Prod:1_hg5Lj-lOXbZMm60FizXSEZBmYN27-ozK-JOX4fRmEWntroxQ"
 - [more]
 -- You might consider having a "Test" project and a "Production" project AppsScirpt file
 -- You can find the project id in the 'Project Settings' > 'IDs' section.
@@ -36,26 +39,29 @@ Add a new Apps Script file location:
 	Del: `
 Delete an Apps Script file location:
 - [how] Select a Apps Script file reference to delete by number from the -list.
-- [syntax] -del Number
-- [example] -del 1
+- [syntax] claspall -del Number
+- [example] 
+    claspall -del 1
 - [more]
 -- You can use the -list flag to get the selected file location to remove
 -- This will not delete the file. It will stop the delete file from being updated from the core file.
-  `,
+    `,
 	Edit: `
 Edit existing Apps Script file locaiton information:
 - [how] Select an Apps Script file reference from the list and update the title and/or file id.
-- [syntax] -edit id:title:Apps-Script-File-Id
-- [example] -edit "1:Prod:1_hg5Lj-lOXbZMm60FizXSEZBmYN27-ozK-JOX4fRmEWntroxQ
+- [syntax] claspall -edit id:title:Apps-Script-File-Id
+- [example] 
+    claspall -edit "1:Prod:1_hg5Lj-lOXbZMm60FizXSEZBmYN27-ozK-JOX4fRmEWntroxQ
 - [more] 
 -- to edit just the Title: "1:New Title:"
 --- Leave out the id after the first colon.
--- To edit just the Apps Script File ID: "1::1_hg5Lj-lOXbZMm60FizXSEZBmYN27-ozK-JOX4fRmEWntroxQ"
+-- To edit just the Apps Script File ID: 
+    "1::1_hg5Lj-lOXbZMm60FizXSEZBmYN27-ozK-JOX4fRmEWntroxQ"
 --- Leave out the name between the id and the Apps Script File ID colon separators.
-  `,
+    `,
 	List: `
-Lists all connected file locations
-  `,
+Lists all connected file locatis
+    `,
 	Version: version,
 }
 
@@ -66,6 +72,9 @@ func NewCmdFlags() *CmdFlags {
 	flag.IntVar(&cf.Del, "del", -1, FlagUsage.Del)
 	flag.StringVar(&cf.Edit, "edit", "", FlagUsage.Edit)
 	flag.BoolVar(&cf.List, "list", false, FlagUsage.List)
+	flag.BoolVar(&cf.List, "ls", false, "See -list")
+	flag.BoolVar(&cf.Version, "version", false, FlagUsage.Version)
+	flag.BoolVar(&cf.Version, "v", false, "See -version")
 
 	flag.Parse()
 
@@ -110,5 +119,7 @@ func (cf CmdFlags) Execute(files *Files) {
 	case cf.Del != -1:
 		files.delete(cf.Del)
 
+	case cf.Version:
+		fmt.Println(version)
 	}
 }
